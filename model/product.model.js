665d8bdac75e3f69a5375c2b6ca823780fc2db94
BesnikBerbatovci19 <51424232+BesnikBerbatovci19@users.noreply.github.com>
2024-05-18 09:37:46 +0200

    update product

--- model/product.model.js
+++ model/product.model.js
@@ -53,21 +53,21 @@ function deleteProduct(id) {
         connection.query(query, [id], (error, results) => {
             if (error) {
                 reject(error);
-            } else { 
+            } else {
                 resolve(results.affectedRows > 0);
             }
         });
     });
 }
 
-function deletePhoto(id, idPhoto){
+function deletePhoto(id, idPhoto) {
     const fetchQuery = "SELECT path FROM product WHERE id = ?";
     const updateQuery = "UPDATE product SET path = ? WHERE id = ?";
 
     return new Promise((resolve, reject) => {
         connection.query(fetchQuery, [id], (error, results) => {
             let remove = JSON.parse(results[0].path).find((item) => item.id == idPhoto);
-            fs.unlink(remove.path, (err) => { if (err) throw err;})
+            fs.unlink(remove.path, (err) => { if (err) throw err; })
             let photo = JSON.parse(results[0].path).filter((item) => item.id != idPhoto);
             connection.query(updateQuery, [JSON.stringify(photo), id], (error, results) => {
                 if (error) {
@@ -79,11 +79,35 @@ function deletePhoto(id, idPhoto){
     });
 }
 
+function updateProduct(id, data, photo) {
+    const updateQuery = `
+    UPDATE product 
+    SET 
+        name = COALESCE(?, name),
+        price = COALESCE(?, price),
+        status = COALESCE(?, status),
+        inStock = COALESCE(?, inStock),
+        warranty = COALESCE(?, warranty),
+        discount = COALESCE(?, discount)
+    WHERE id = ?
+`;
+
+    return new Promise((resolve, reject) => {
+        connection.query(updateQuery, [data.name, data.price, data.status, data.inStock, data.warranty, data.discount, id], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.affectedRows > 0);
+            }
+        });
+    });
+}
 
 module.exports = {
     getAllProduct,
     getProductById,
     createProduct,
     deleteProduct,
-    deletePhoto
+    deletePhoto,
+    updateProduct
 }
